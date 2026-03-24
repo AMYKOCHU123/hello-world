68e63b5aeb840f530ad4953138f6c0e10516c9c0
Amal C <amalch1993@gmail.com>
2026-03-24 15:56:22 +0530

    updated pom

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -5,13 +5,10 @@ import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import java.io.BufferedWriter;
 
-/**
- * A simple HTTP function that responds with "Hello, World from Cloud Run!"
- */
 public class HelloWorld implements HttpFunction {
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
         BufferedWriter writer = response.getWriter();
-        writer.write("Hello, World from Cloud Run!");
+        writer.write("Hello, Cloud Function!");
     }
 }
